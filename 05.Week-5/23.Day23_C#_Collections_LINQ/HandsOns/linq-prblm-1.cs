b0a6c8bea0564f427c5bdd9b2dac1959dbb4a44a
emanibrahmanjali-28 <20pa1a1228@vishnu.edu.in>
2026-03-20 09:43:57 +0530

    Update linq-prblm-1.cs

--- 05.Week-5/23.Day23_C#_Collections_LINQ/HandsOns/linq-prblm-1.cs
+++ 05.Week-5/23.Day23_C#_Collections_LINQ/HandsOns/linq-prblm-1.cs
@@ -11,9 +11,19 @@ namespace LinqCodeTemplate
 
             var products = product.GetProducts();
 
-            var result = products
-                         .Where(p => p.ProCategory == "FMCG")
-                         .ToList();
+            // var result = products
+            //              .Where(p => p.ProCategory == "FMCG")
+            //              .ToList();
+
+            // foreach (var item in result)
+            // {
+            //     Console.WriteLine($"{item.ProCode}\t{item.ProName}\t{item.ProMrp}");
+            // }
+
+
+            var result = from p in products
+             where p.ProCategory == "FMCG"
+             select p;
 
             foreach (var item in result)
             {
