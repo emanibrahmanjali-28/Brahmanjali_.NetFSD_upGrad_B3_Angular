276bf5f46b7efe1e31776da6a2dca57c78817ae8
emanibrahmanjali-28 <20pa1a1228@vishnu.edu.in>
2026-03-20 09:47:18 +0530

    Update linq-prblm-3.cs

--- 05.Week-5/23.Day23_C#_Collections_LINQ/HandsOns/linq-prblm-3.cs
+++ 05.Week-5/23.Day23_C#_Collections_LINQ/HandsOns/linq-prblm-3.cs
@@ -12,9 +12,13 @@ namespace LinqCodeTemplate
             var products = product.GetProducts();
 
             
-            var result = products
-                         .OrderBy(p => p.ProCode)
-                         .ToList();
+            // var result = products
+            //              .OrderBy(p => p.ProCode)
+            //              .ToList();
+
+            var result = from p in products
+             orderby p.ProCode ascending
+             select p;
 
             foreach (var item in result)
             {
