10fa95db9ec32990926964993070f96402dcdb90
emanibrahmanjali-28 <20pa1a1228@vishnu.edu.in>
2026-03-25 08:16:56 +0530

    Add files via upload

---
+++ 06.Week-6/26.Day26_ADO.NET/HandsOns/StoredProcedures-day26.sql
@@ -0,0 +1,64 @@
+CREATE DATABASE ProductDB;
+GO
+
+USE ProductDB;
+
+CREATE TABLE Products
+(
+    ProductId INT IDENTITY(1,1) PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    Price DECIMAL(10,2)
+);
+
+
+CREATE PROCEDURE sp_InsertProduct
+    @ProductName VARCHAR(100),
+    @Category VARCHAR(50),
+    @Price DECIMAL(10,2)
+AS
+BEGIN
+    INSERT INTO Products(ProductName, Category, Price)
+    VALUES(@ProductName, @Category, @Price);
+END
+
+
+
+CREATE PROCEDURE sp_GetAllProducts
+AS
+BEGIN
+    SELECT * FROM Products;
+END
+
+
+CREATE PROCEDURE sp_UpdateProduct
+    @ProductId INT,
+    @ProductName VARCHAR(100),
+    @Category VARCHAR(50),
+    @Price DECIMAL(10,2)
+AS
+BEGIN
+    UPDATE Products
+    SET ProductName = @ProductName,
+        Category = @Category,
+        Price = @Price
+    WHERE ProductId = @ProductId;
+END
+
+
+CREATE PROCEDURE sp_DeleteProduct
+    @ProductId INT
+AS
+BEGIN
+    DELETE FROM Products WHERE ProductId = @ProductId;
+END
+
+
+CREATE PROCEDURE sp_GetProductById
+    @ProductId INT
+AS
+BEGIN
+    SELECT * FROM Products WHERE ProductId = @ProductId;
+END
+
+SELECT * FROM Products
